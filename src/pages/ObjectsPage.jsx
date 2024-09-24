cc2a23a875e4b07374bc243f93e52804bc5b41c5
DenisCoder1 <denisbiryuchinskiy251606@gmail.com>
2024-09-24 19:21:53 +0300

    add

--- src/pages/ObjectsPage.jsx
+++ src/pages/ObjectsPage.jsx
@@ -24,7 +24,7 @@ export default function ObjectsPage() {
                   <p>ул. Нижненольная, 13</p>
               </div>
               <div className='objects__image'>
-                  <Link to='objects/townhouse'> <img src='object__1.png'/> </Link>
+                  <Link to='/objects/townhouse'> <img src='object__1.png'/> </Link>
                   <h2>3-х этажный таунхаус бизнес-класса</h2>
                   <p>ул. Нижненольная, 13A</p>
               </div>
