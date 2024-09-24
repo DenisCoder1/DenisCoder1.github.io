e448443a40d3f636d3d8df671768f97ce5d2b462
DenisCoder1 <denisbiryuchinskiy251606@gmail.com>
2024-09-24 16:14:59 +0300

    commit

--- src/components/Objects.jsx
+++ src/components/Objects.jsx
@@ -8,12 +8,12 @@ function Objects() {
         <div className='objects'>
             
             <div className='objects__image'>
-                <Link to="/objects/townhouse"><img src='object__1.png'/></Link>
+                <a href="/objects/townhouse"><img src='object__1.png'/></a>
                 <h2>3-х этажный таунхаус бизнес-класса</h2>
                 <p>ул. Нижненольная, 13</p>
             </div>
             <div className='objects__image'>
-                <Link to="/objects/townhouse"><img src='object__1.png'/></Link>
+                <a href="/objects/townhouse"><img src='object__1.png'/></a>
                 <h2>3-х этажный таунхаус бизнес-класса</h2>
                 <p>ул. Нижненольная, 13A</p>
             </div>
