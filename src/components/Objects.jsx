8988d9f755ed20829f45e9d445152aeffa8ad611
DenisCoder1 <denisbiryuchinskiy251606@gmail.com>
2024-09-24 16:53:57 +0300

    add

--- src/components/Objects.jsx
+++ src/components/Objects.jsx
@@ -12,7 +12,7 @@ function Objects() {
             <div className='objects__image'>
                 <Link to="/objects/townhouse"><img src='object__1.png'/></Link>
                 <h2>3-х этажный таунхаус бизнес-класса</h2>
-                <p>ул. Нижненольная, 13</p>
+                <p>ул. Нижненольная, 13A</p>
             </div>
             <div className='objects__image'>
                 <Link to="/objects/townhouse"><img src='object__1.png'/></Link>
